ea9dae4a5df078a911c2137f5830c4ea3701c91f
Administrator <921497786@qq.com>
2021-09-15 15:46:58 +0800

    9/15

---
+++ venv/test_1/tc5.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+pd.set_option('display.unicode.east_asian_width', True)
+df = pd.read_csv('test.csv')
+
+for d in df['数学']:
+    if d < 100:
+        # print(d.index)
+        pass
+
+print(df['英语']>110)
+# print(df[df['英语']>110].index)
---
+++ venv/test_1/test_datafram.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+pd.set_option('display.unicode.east_asian_width', True)
+df = pd.DataFrame({
+    '语文':[110,105,99],
+    '数学':[105,88,115],
+    '英语':[109,120,130],
+    '班级':'高一7班'
+},index=[0,1,2])
+
+print(df['语文'])
+
+pd.read_csv()
