171e67cce1ae01bd83d4d08bf03d1001ace8c99f
Administrator <921497786@qq.com>
2021-09-17 14:16:25 +0800

    modified:   tc5.py 	modified:   test_datafram.py

--- venv/test_1/tc5.py
+++ venv/test_1/tc5.py
@@ -2,12 +2,22 @@ import pandas as pd
 
 pd.set_option('display.unicode.east_asian_width', True)
 df = pd.read_csv('test.csv')
+df['物理'] = [88,79,60]
+df = df.set_index(['学号'],drop=True)
+print(df)
 
-for d in df['数学']:
-    if d < 100:
-        # print(d.index)
-        pass
+print('===============')
+
+print(df['数学'][110])
 
 
-print(df['英语']>110)
+print('===============')
+print(df.at[110,'数学'])
+
+print('===============')
+print(df['语文']<110)
+print(df[df['语文']<110].index)
+
+print(type(df['语文']))
+# print(df['英语']>110)
 # print(df[df['英语']>110].index)
--- venv/test_1/test_datafram.py
+++ venv/test_1/test_datafram.py
@@ -2,13 +2,12 @@ import pandas as pd
 
 pd.set_option('display.unicode.east_asian_width', True)
 df = pd.DataFrame({
-    '语文':[110,105,99],
-    '数学':[105,88,115],
-    '英语':[109,120,130],
-    '班级':'高一7班'
-},index=[0,1,2])
+    '语文':[110,105,99,120],
+    '数学':[105,88,115,118],
+    '英语':[109,120,130,98],
+    '班级':['高一7班']
+},index=[0,1,2,3])
 
 
-print(df['语文'])
+print(df)
 
-pd.read_csv()
